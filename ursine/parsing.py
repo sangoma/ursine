0aebd876dbbf370c46a4b5dde85947679d898e82
Terry Kerr <t@xnr.ca>
2018-04-23 12:44:56 -0400

    comments/docstrings for parsing

--- ursine/parsing.py
+++ ursine/parsing.py
@@ -1,29 +1,46 @@
+'''Regex based parsing for sip uris.'''
 import re
 import multidict
 
 
+# optionally extract either a quoted or unquoted
+# contact name and extract the rest discarding the <>
+# brackets if present
 contact_re = re.compile(r'("(?P<quoted>[^"]+)" '
                         r'|(?P<unquoted>[^\<"]+) )?'
                         r'\<(?P<the_rest>.*)\>\Z')
 
+# extract a scheme, discard the : and extract the rest
 scheme_re = re.compile(r'(?P<scheme>[^:]+):(?P<the_rest>.*)')
 
+# optionally extract a user part (either user
+# or user:pass) and discard the @ symbol
 user_part_re = re.compile(r'(?P<user_part>[^:@]+(:[^@:]+)?)@(?P<the_rest>.*)')
 
+# extract a host component that may be [...]
+# delimited in order to support ipv6 hosts
 host_re = re.compile(r'(?P<host>('
                      r'\[[^\]]+])|([^:|;]+)'
                      r')(?P<the_rest>.*)')
 
+# optionally discard a : and extract a port number
 port_re = re.compile(r':(?P<port>[^;?]*)(?P<the_rest>.*)')
 
+# optionally discard a ; and extract a single key=val pair
 param_re = re.compile(r';(?P<key>[^=]+)=(?P<val>[^;?]*)(?P<the_rest>.*)')
 
+# optionally extract a key=val pair
 base_header_re = r'(?P<key>[^=]+)=(?P<val>[^&]*)(?P<the_rest>.*)'
+# match a ? then a key=val pair - only first header starts with ?
 first_header_re = re.compile(rf'\?{base_header_re}')
+# match a & then a key=val pair - matches (n>1)th headers
 nth_header_re = re.compile(rf'&{base_header_re}')
 
 
 def parse_contact(uri):
+    '''Attempt a contact match, and remove the
+    proper sip uri from a <> pair if present
+    '''
     match = contact_re.match(uri)
     if not match:
         return None, uri
@@ -37,6 +54,7 @@ def parse_contact(uri):
 
 
 def parse_scheme(uri):
+    '''Require a scheme match.'''
     match = scheme_re.match(uri)
     if match is None:
         raise ValueError('no scheme specified')
@@ -50,6 +68,7 @@ def parse_scheme(uri):
 
 
 def parse_user_part(uri):
+    '''Attempt to match user@ or user:pass@.'''
     match = user_part_re.match(uri)
     if match is None:
         return None, None, uri
@@ -65,6 +84,13 @@ def parse_user_part(uri):
 
 
 def parse_host(uri):
+    '''Require a host match.
+
+    The host can either be anything ending
+    with the EOL, a ; or a :, and optionally
+    can be enclosed in [...] to allow
+    containing : characters.
+    '''
     match = host_re.match(uri)
     if match is None:
         raise ValueError('host must be specified')
@@ -73,6 +99,7 @@ def parse_host(uri):
 
 
 def parse_port(uri):
+    '''Attempt to match a : and valid port number.'''
     match = port_re.match(uri)
     if match is None:
         return None, uri
@@ -90,6 +117,7 @@ def parse_port(uri):
 
 
 def parse_parameters(uri):
+    '''Attempt to match many ;key=val pairs.'''
     the_rest = uri
     params = {}
 
@@ -106,6 +134,9 @@ def parse_parameters(uri):
 
 
 def parse_headers(uri):
+    '''Attempt to match a ?key=val pair, then
+    attempt to match many &key=val pairs.
+    '''
     the_rest = uri
     headers = multidict.MultiDict()
 
