d727df46c002dd95e9d88216049f23d3dbe8dd47
Terry Kerr <t@xnr.ca>
2018-04-25 16:10:05 -0400

    v0.2.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ class PyTest(TestCommand):
 
 
 setup(name='ursine',
-      version='0.2',
+      version='0.2.1',
       description='library for SIP url handling/maninupation',
       long_description=readme(),
       long_description_content_type='text/markdown',
