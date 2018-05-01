7a94c5fba9ea91319195500e3b38411c7f84a739
Terry Kerr <t@xnr.ca>
2018-05-01 16:38:50 -0400

    update tests to parsing/uri reworks

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,114 +1,51 @@
 import pytest
-from ursine.parsing import (
-    parse_contact,
-    parse_scheme,
-    parse_user_part,
-    parse_host,
-    parse_port,
-    parse_parameters,
-    parse_headers,
-)
+from ursine.parsing import parse
 
 
-@pytest.mark.parametrize('uri,expect_contact,expect_the_rest', [
-    ('sip:host', None, 'sip:host'),
-    ('<sip:host>', None, 'sip:host'),
-    ('John <sip:host>', 'John', 'sip:host'),
-    ('"Mary" <sip:host>', 'Mary', 'sip:host'),
-    ('"Alice Cooper" <sip:host>', 'Alice Cooper', 'sip:host'),
-    ('"<>>:<<>" <sip:host>', '<>>:<<>', 'sip:host'),
-    ('<sip:host', None, '<sip:host'),
-    ('sip:host>', None, 'sip:host>'),
-    ('Mary" <sip:host>', None, 'Mary" <sip:host>'),
-    ('Alice sip:host', None, 'Alice sip:host'),
-    (' sip:host', None, ' sip:host'),
+@pytest.mark.parametrize('uri,expect', [
+    ('sip:localhost', 'sip'),
+    ('sips:localhost', 'sips'),
 ])
-def test_contact(uri, expect_contact, expect_the_rest):
-    contact, the_rest = parse_contact(uri)
-    assert contact == expect_contact
-    assert the_rest == expect_the_rest
-
-
-@pytest.mark.parametrize('uri,expect_scheme,expect_the_rest', [
-    ('sip:', 'sip', ''),
-    ('sips:', 'sips', ''),
-    ('sip:localhost:5060', 'sip', 'localhost:5060'),
-])
-def test_scheme(uri, expect_scheme, expect_the_rest):
-    scheme, the_rest = parse_scheme(uri)
-    assert scheme == expect_scheme
-    assert the_rest == expect_the_rest
+def test_scheme(uri, expect):
+    assert parse(uri).scheme == expect
 
 
 @pytest.mark.parametrize('uri', [
-    '', 'sisp:', ':', 'sip', 'sips', 'localhost:5060',
+    '',
+    ':',
+    ':localhost',
 ])
-def test_scheme_fails(uri):
+def test_scheme_fail(uri):
     with pytest.raises(ValueError):
-        parse_scheme(uri)
+        parse(uri)
 
 
-@pytest.mark.parametrize('uri,expect_user,expect_pass,expect_the_rest', [
-    ('bob:pass@some.tld', 'bob', 'pass', 'some.tld'),
-    ('bob@some.tld', 'bob', None, 'some.tld'),
-    ('some.tld', None, None, 'some.tld'),
-    ('a@some.tld', 'a', None, 'some.tld'),
-    ('@some.tld', None, None, '@some.tld'),
-    (':some.tld', None, None, ':some.tld'),
+@pytest.mark.parametrize('uri,expect', [
+    ('sip:localhost', 'localhost'),
+    ('sip:127.0.0.1', '127.0.0.1'),
+    ('sip:[::dead:beef]', '[::dead:beef]'),
+    ('sip:localhost:5060', 'localhost:5060'),
+    ('sip:127.0.0.1:5060', '127.0.0.1:5060'),
+    ('sip:[::dead:beef]:5060', '[::dead:beef]:5060'),
 ])
-def test_user_part(uri, expect_user, expect_pass, expect_the_rest):
-    user, password, the_rest = parse_user_part(uri)
-    assert user == expect_user
-    assert password == expect_pass
-    assert the_rest == expect_the_rest
+def test_hostport(uri, expect):
+    assert parse(uri).hostport == expect
 
 
-@pytest.mark.parametrize('uri,expect_host,expect_the_rest', [
-    ('some.tld', 'some.tld', ''),
-    ('some.tld:5060;x=y', 'some.tld', ':5060;x=y'),
-    ('tld', 'tld', ''),
-    ('tld:5060;x=y', 'tld', ':5060;x=y'),
-    ('[::1]:5060', '[::1]', ':5060'),
+@pytest.mark.parametrize('uri', [
+    'sip:',
+    'sip:?',
+    'sip:;',
 ])
-def test_host(uri, expect_host, expect_the_rest):
-    host, the_rest = parse_host(uri)
-    assert host == expect_host
-    assert the_rest == expect_the_rest
+def test_hostport_fail(uri):
+    with pytest.raises(ValueError):
+        parse(uri)
 
 
-@pytest.mark.parametrize('uri,expect_port,expect_the_rest', [
-    (':5060', 5060, ''),
-    (':1', 1, ''),
-    (':5060;x=y', 5060, ';x=y'),
-    (':5060?x=y', 5060, '?x=y'),
-    (':65535', 65535, ''),
+@pytest.mark.parametrize('uri,expect', [
+    ('sip:user@localhost', 'user'),
+    ('sip:user:pass@localhost', 'user:pass'),
+    ('sip:u:p@localhost', 'u:p'),
 ])
-def test_port(uri, expect_port, expect_the_rest):
-    port, the_rest = parse_port(uri)
-    assert port == expect_port
-    assert the_rest == expect_the_rest
-
-
-@pytest.mark.parametrize('uri,expect_params,expect_the_rest', [
-    (';ten=10', {'ten': '10'}, ''),
-    (';ten=10;five=5', {'ten': '10', 'five': '5'}, ''),
-    (';ten=10;five=5?x=y', {'ten': '10', 'five': '5'}, '?x=y'),
-])
-def test_parameters(uri, expect_params, expect_the_rest):
-    params, the_rest = parse_parameters(uri)
-    assert params == expect_params
-    assert the_rest == expect_the_rest
-
-
-@pytest.mark.parametrize('uri,expect_headers,expect_the_rest', [
-    ('?x=y', {'x': ['y']}, ''),
-    ('?x=y&x=z', {'x': ['y', 'z']}, ''),
-    ('?x=y&a=b', {'x': ['y'], 'a': ['b']}, ''),
-    ('?x=&y=', {'x': [''], 'y': ['']}, ''),
-])
-def test_headers(uri, expect_headers, expect_the_rest):
-    headers, the_rest = parse_headers(uri)
-    for k, v in expect_headers.items():
-        assert headers.popall(k) == v
-    assert len(headers) == 0
-    assert the_rest == expect_the_rest
+def test_userinfo(uri, expect):
+    assert parse(uri).userinfo == expect
