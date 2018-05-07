5fb19854fe1d92106c0d556c5b9220de6f953c4d
Terry Kerr <t@xnr.ca>
2018-05-07 13:31:46 -0400

    add Header utilities (repr, eq, hash, deepcopy, build)

--- tests/test_header.py
+++ tests/test_header.py
@@ -9,3 +9,17 @@ from ursine import Header, URI
 ])
 def test_str(header, expect):
     assert str(Header(header)) == expect
+
+
+@pytest.mark.parametrize('uri,display_name,params,expect', [
+    (URI('sip:localhost'), 'John', {}, '"John" <sip:localhost>'),
+    (URI('sip:localhost;x=y'), 'John', {}, '"John" <sip:localhost;x=y>'),
+    (URI('sip:localhost'), 'John', {'x': 'y'}, '"John" <sip:localhost>;x=y'),
+    (URI('sip:localhost;x=y'), 'John', {'x': 'y'}, '"John" <sip:localhost;x=y>;x=y'),
+    (URI('sip:localhost'), None, {}, '<sip:localhost>'),
+])
+def test_build(uri, display_name, params, expect):
+    header = Header.build(uri=uri,
+                          display_name=display_name,
+                          parameters=params)
+    assert header == Header(expect)
